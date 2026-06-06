09eba80276c9219a204d834631898d04d1a14191
Subhansee Yadav <subhanseey@gmail.com>
2026-06-06 15:42:39 +0530

    hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -60,18 +60,89 @@ class ModelTrainer:
                 "AdaBoost": AdaBoostRegressor(),
             }
 
+            params = {
+
+                "Decision Tree": {
+                    "criterion": [
+                        "squared_error",
+                        "friedman_mse",
+                        "absolute_error",
+                        "poisson"
+                    ],
+                    "max_features": [
+                        "sqrt",
+                        "log2"
+                    ]
+                },
+
+                "Random Forest": {
+                    "n_estimators": [
+                        8, 16, 32, 64, 128, 256
+                    ]
+                },
+
+                "Gradient Boosting": {
+                    "learning_rate": [
+                        0.1, 0.01, 0.05, 0.001
+                    ],
+                    "subsample": [
+                        0.6, 0.7, 0.75, 0.8, 0.85, 0.9
+                    ],
+                    "n_estimators": [
+                        8, 16, 32, 64, 128, 256
+                    ]
+                },
+
+                "Linear Regression": {},
+
+                "K-Neighbors Regressor": {
+                    "n_neighbors": [
+                        5, 7, 9, 11
+                    ]
+                },
+
+                "XGBoost": {
+                    "learning_rate": [
+                        0.1, 0.01, 0.05, 0.001
+                    ],
+                    "n_estimators": [
+                        8, 16, 32, 64, 128, 256
+                    ]
+                },
+
+                "CatBoost": {
+                    "depth": [
+                        6, 8, 10
+                    ],
+                    "learning_rate": [
+                        0.01, 0.05, 0.1
+                    ],
+                    "iterations": [
+                        30, 50, 100
+                    ]
+                },
+
+                "AdaBoost": {
+                    "learning_rate": [
+                        0.1, 0.01, 0.5, 0.001
+                    ],
+                    "n_estimators": [
+                        8, 16, 32, 64, 128, 256
+                    ]
+                }
+            }
+
             model_report = evaluate_models(
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
-                models=models
+                models=models,
+                params=params
             )
 
-            # Best model score
             best_model_score = max(model_report.values())
 
-            # Best model name
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
@@ -95,7 +166,10 @@ class ModelTrainer:
 
             predicted = best_model.predict(X_test)
 
-            r2_square = r2_score(y_test, predicted)
+            r2_square = r2_score(
+                y_test,
+                predicted
+            )
 
             return r2_square
 
--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,12 @@ import sys
 import dill
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
 
+# ---------------- SAVE OBJECT ----------------
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -20,31 +22,37 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+# ---------------- EVALUATE MODELS ----------------
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
-
         report = {}
 
-        for i in range(len(models)):
+        for model_name, model in models.items():
 
-            model = list(models.values())[i]
+            para = params[model_name]
 
-            # Train model
+            gs = GridSearchCV(
+                model,
+                para,
+                cv=3,
+                n_jobs=-1,
+                verbose=1
+            )
+
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
-            # Predictions
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
-            # Scores
-            train_model_score = r2_score(y_train, y_train_pred)
-            test_model_score = r2_score(y_test, y_test_pred)
+            test_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_score
 
         return report
 
     except Exception as e:
         raise CustomException(e, sys)
-    
     
