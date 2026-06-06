baaa1bec10696712017a8f00837f781e5da1250c
Subhansee Yadav <subhanseey@gmail.com>
2026-06-06 17:56:51 +0530

    Deployment Configuration

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,15 @@
 import os
 import sys
-import dill
 
+import numpy as np 
+import pandas as pd
+import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
-
-# ---------------- SAVE OBJECT ----------------
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -16,43 +17,46 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
-
-
-# ---------------- EVALUATE MODELS ----------------
-def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
-        for model_name, model in models.items():
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            para = params[model_name]
-
-            gs = GridSearchCV(
-                model,
-                para,
-                cv=3,
-                n_jobs=-1,
-                verbose=1
-            )
-
-            gs.fit(X_train, y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train, y_train)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
 
-            test_score = r2_score(y_test, y_test_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
 
-            report[model_name] = test_score
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
 
     except Exception as e:
         raise CustomException(e, sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
