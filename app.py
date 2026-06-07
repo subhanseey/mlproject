8989aa0fd7c98d3e16bbd9a341c252cce653c86e
Subhansee Yadav <subhanseey@gmail.com>
2026-06-07 17:43:21 +0530

    Use Render port and disable debug mode

--- app.py
+++ app.py
@@ -45,6 +45,9 @@ def predict_datapoint():
             return "Something went wrong in prediction"
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     
